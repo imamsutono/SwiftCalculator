bb615a67d61b417d31fff30a5ad7f8c9eeff98e6
imamsutono <imamssutono@gmail.com>
2018-08-19 17:16:54 +0700

    Add button with alert and actionsheet

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,6 +15,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var btn: UIButton!
+    @IBAction func klikButton(_ sender: UIButton) {
+        //let alert = UIAlertController(title: "Ini Judul", message: "Ini pesannya", preferredStyle: UIAlertControllerStyle.alert)
+        let alert = UIAlertController(title: "Ini Judul", message: "Ini pesannya", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        // add action di button alert
+        let actionOk = UIAlertAction(title: "OK", style: .default) { (aksiButton) in
+            print("aksi klik button")
+        }
+        
+        alert.addAction(actionOk)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
